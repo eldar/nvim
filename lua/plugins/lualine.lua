7584283db10f227efb55d22241084f4ae54cd608
Eldar Insafutdinov <e.insafutdinov@gmail.com>
2024-08-23 18:06:26 +0100

    center harpoon in the middle of the status line

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -3,6 +3,7 @@ return {
     'nvim-lualine/lualine.nvim',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     opts = {
+      component_separators = { left = '', right = '' },
       sections = {
         -- Other Status Line components
         -- lualine_a = { ... },
@@ -10,10 +11,17 @@ return {
           {
             "filename",
             path = 1,
-          }
+          },
         },
         lualine_c = {
           {
+            '%=',
+            -- separator=nil,
+            separator = { left = "", right = ""},
+          },
+          {
+            -- separator = { left = "", right = ""},
+            -- icon = nil,
             "harpoon2",
             -- icon = '',
           }
