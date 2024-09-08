20421d16d2357a4d3a4a26bcff6e637699ed7622
Eldar Insafutdinov <e.insafutdinov@gmail.com>
2024-09-08 12:16:11 +0100

    New plugins: barbecue and lspkind

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -45,6 +45,7 @@ require('mason-lspconfig').setup({
                   diagnosticSeverityOverrides = {
                     reportDuplicateImport = "warning",
                   },
+                  logLevel = "Trace",
                   -- diagnosticSeverityOverrides = {
                   --   -- https://github.com/detachhead/basedpyright/blob/main/docs/configuration.md
                   --   reportUndefinedVariable = "error",
@@ -108,4 +109,24 @@ cmp.event:on(
   cmp_autopairs.on_confirm_done()
 )
 
+local lspkind = require('lspkind')
+cmp.setup {
+  formatting = {
+    format = lspkind.cmp_format({
+      mode = 'symbol', -- show only symbol annotations
+      maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+                     -- can also be a function to dynamically calculate max width such as 
+                     -- maxwidth = function() return math.floor(0.45 * vim.o.columns) end,
+      ellipsis_char = '...', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
+      show_labelDetails = true, -- show labelDetails in menu. Disabled by default
+
+      -- The function below will be called before any actual modifications from lspkind
+      -- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
+      -- before = function (entry, vim_item)
+      --   ...
+      --   return vim_item
+      -- end
+    })
+  }
+}
 require("lsp_signature").setup()
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -44,4 +44,33 @@ return {
         -- Your setup opts here
       },
     },
+  {
+    "onsails/lspkind.nvim",
+    opts = {
+      mode = "symbol",
+      symbol_map = {
+        Array = "󰅪",
+        Boolean = "⊨",
+        Class = "󰌗",
+        Constructor = "",
+        Key = "󰌆",
+        Namespace = "󰅪",
+        Null = "NULL",
+        Number = "#",
+        Object = "󰀚",
+        Package = "󰏗",
+        Property = "",
+        Reference = "",
+        Snippet = "",
+        String = "󰀬",
+        TypeParameter = "󰊄",
+        Unit = "",
+      },
+      menu = {},
+    },
+    enabled = vim.g.icons_enabled,
+    config = function(_, opts)
+      require("lspkind").init(opts)
+    end,
+  },
 }
