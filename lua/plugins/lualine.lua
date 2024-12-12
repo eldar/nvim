3b6217f8173f3ef3736aa4cd1801f6b108b78a55
Eldar Insafutdinov <e.insafutdinov@gmail.com>
2024-12-12 08:28:58 +0000

    add word count for markdown files

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -2,36 +2,45 @@ return {
   {
     'nvim-lualine/lualine.nvim',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
+    config = function ()
+      local prose = require("nvim-prose")
+      local lualine = require("lualine")
+      lualine.setup({
+        component_separators = { left = '', right = '' },
+        sections = {
+          -- Other Status Line components
+          -- lualine_a = { ... },
+          lualine_b = {
+            {
+              "filename",
+              path = 1,
+            },
+          },
+          lualine_c = {
+            {
+              '%=',
+              separator = { left = "", right = ""},
+            },
+            {
+              "harpoon2",
+            }
+          },
+          lualine_x = {
+            { prose.word_count, cond = prose.is_available },
+            'filetype'
+          },
+          -- lualine_c = {
+          --   {
+          --     function()
+          --       -- invoke `progress` here.
+          --       return require('lsp-progress').progress()
+          --     end,
+          --   },
+          -- },
+        }
+      })
+    end,
     opts = {
-      component_separators = { left = '', right = '' },
-      sections = {
-        -- Other Status Line components
-        -- lualine_a = { ... },
-        lualine_b = {
-          {
-            "filename",
-            path = 1,
-          },
-        },
-        lualine_c = {
-          {
-            '%=',
-            separator = { left = "", right = ""},
-          },
-          {
-            "harpoon2",
-          }
-        },
-        lualine_x = {'filetype'},
-        -- lualine_c = {
-        --   {
-        --     function()
-        --       -- invoke `progress` here.
-        --       return require('lsp-progress').progress()
-        --     end,
-        --   },
-        -- },
-      }
     }
   },
 }
