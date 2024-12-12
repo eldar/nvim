2cad61e788f451c292ca1b31105d8fc7296a8baa
Eldar Insafutdinov <e.insafutdinov@gmail.com>
2024-12-12 13:02:04 +0000

    extra markdown settings

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -1,3 +1,10 @@
+local setlocal = vim.opt_local
+
+setlocal.expandtab = true
+setlocal.ts = 2
+setlocal.sts = 2
+setlocal.sw = 2
+
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.softtabstop = 2
